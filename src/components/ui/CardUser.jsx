c440ab6cacf2b01a6026e535fd1469cf1c9d5d7e
Sandro <sandro.gomes@aluno.ifsp.edu.br>
2023-09-18 18:00:08 -0300

    18-09-2023

---
+++ src/components/ui/CardUser.jsx
@@ -0,0 +1,15 @@
+import './CardUser.css'
+ const cardUser = () => {
+  return (
+    <div className="card-user">
+        <img src="" alt="sandro" />
+        <div>
+        <h3>Sandro</h3>
+        <span>Sandro.gomes@aluno.edu.ifsp.br</span>
+        </div>
+    </div>
+   
+  )
+}
+
+export default cardUser
