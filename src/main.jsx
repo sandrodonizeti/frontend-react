47da477c02138ed1a983b1dbfdde1ade4cac6d20
sandrodonizeti <sandro.gomes@aluno.ifsp.edu.br>
2023-10-09 12:03:09 -0300

    casa-09-10-2023

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-  <React.StrictMode>
-    <BrowserRouter>
+      <BrowserRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    </BrowserRouter>,
 )
